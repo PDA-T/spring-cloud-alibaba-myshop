6abfc78a3080ae97cce9027469fb7dc2ae6a7634
PDA-T <1581109028@qq.com>
2022-11-22 19:38:59 +0800

    用户注册application

---
+++ myshop-service-reg/src/main/java/com/pda/myshop/service/reg/MyShopServiceRegApplication.java
@@ -0,0 +1,20 @@
+package com.pda.myshop.service.reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @Date 2022/11/22 18:40
+ * @Description 用户注册
+ * @since version-1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient// 注册进nacos
+@MapperScan(basePackages = "com.pda.myshop.commons.mapper")// 扫描mapper
+public class MyShopServiceRegApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopServiceRegApplication.class,args);
+	}
+}
