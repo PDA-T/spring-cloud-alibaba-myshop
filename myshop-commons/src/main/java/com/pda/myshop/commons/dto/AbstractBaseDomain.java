3bd0f414d61cec6de878ddeec3c5b15bd7965baf
PDA-T <1581109028@qq.com>
2022-11-26 19:25:19 +0800

    通用领域模型

--- myshop-commons/src/main/java/com/pda/myshop/commons/dto/AbstractBaseDomain.java
+++ myshop-commons/src/main/java/com/pda/myshop/commons/dto/AbstractBaseDomain.java
@@ -1,8 +1,13 @@
 package com.pda.myshop.commons.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Date 2022/11/25 22:32
@@ -12,5 +17,15 @@ import java.io.Serializable;
 @Data
 public abstract class AbstractBaseDomain implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	// 创建时间
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+	private Date created;
+
+	// 更新时间
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+	private Date updated;
 }
